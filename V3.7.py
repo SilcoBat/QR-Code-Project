951d21f8d40c768070410566964e90a4d56a4673
SilcoBat <nikolas.trencik@silcoteceurope.com>
2024-10-17 09:11:50 +0200

    Update V3.7.py

--- V3.7.py
+++ V3.7.py
@@ -1273,7 +1273,7 @@ class MainPage(ttk.Frame):
 
             simple_pn_id = self.master.execute_query(
 
-                "SELECT ID FROM WorkOrders WHERE PN=%s AND WO=%s", (pn_value,self.wo_vaue,),
+                "SELECT ID FROM WorkOrders WHERE PN=%s AND WO=%s", (pn_value, wo_vaue,),
 
                 fetchone=True, caller="check_all_sub_levels_completed"
 
